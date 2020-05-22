4d1c0b88d2113bf15d5f6f079df1e8dd6de28a00
Razvan-Andrei Manea <razvan.razvanblue@gmail.com>
2020-05-22 22:31:04 +0300

    Added whole project

---
+++ Assets/Rocket.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rocket : MonoBehaviour {
+
+    [SerializeField] float mainThrust = 1000f;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float levelLoadDelay = 2f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip death;
+    [SerializeField] AudioClip success;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem deathParticles;
+    [SerializeField] ParticleSystem successParticles;
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+    enum State
+    {
+        Alive,
+        Dying,
+        Transcending
+    }
+    State state = State.Alive;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (state == State.Alive)
+        {
+            HandleThrusting();
+            HandleRotation();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (state != State.Alive) { return; }
+
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":        // do nothing
+                break;
+            case "Finish":
+                StartSuccessSequence();
+                break;
+            default:
+                StartDeathSequence();
+                break;
+        }
+    }
+
+    private void StartSuccessSequence()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        successParticles.Play();
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+
+    private void StartDeathSequence()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        deathParticles.Play();
+        Invoke("LoadFirstLevel", levelLoadDelay);
+    }
+    
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCountInBuildSettings;
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    private void HandleThrusting()
+    {
+        float currentFrameThrust = mainThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.PlayOneShot(mainEngine);
+            }
+            mainEngineParticles.Play();
+            rigidBody.AddRelativeForce(Vector3.up * currentFrameThrust);
+        }
+        else
+        {
+            audioSource.Stop();
+            mainEngineParticles.Stop();
+        }
+    }
+    private void HandleRotation()
+    {
+        rigidBody.angularVelocity = Vector3.zero;        //remove rotation due to physics
+        float currentFrameRotation = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * currentFrameRotation);
+            //rigidBody.AddRelativeTorque(Vector3.forward / 2);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.back * currentFrameRotation);
+            //rigidBody.AddRelativeTorque(Vector3.back / 2);
+        }
+
+        //rigidBody.freezeRotation = false;       //resume physics control
+    }
+
+}
---
+++ Assets/Score.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+    [SerializeField] GameObject start;
+    [SerializeField] GameObject finish;
+    [SerializeField] GameObject player1;
+    [SerializeField] GameObject player2;
+
+    int score1 = 0;
+    int score2 = 0;
+    int maxScore = 0;
+
+    // Use this for initialization
+    void Start () {
+        maxScore = (int)((finish.transform.position - start.transform.position).x
+            - finish.GetComponent<MeshFilter>().mesh.bounds.size.x * finish.transform.localScale.x / 2);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        int deltaX1 = (int) (player1.transform.position - start.transform.position).x;
+        score1 = deltaX1 > score1 ? Mathf.Min(deltaX1, maxScore) : score1;
+
+        int deltaX2 = (int) (player2.transform.position - start.transform.position).x;
+        score2 = deltaX2 > score2 ? Mathf.Min(deltaX2, maxScore) : score2;
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 150, 20), "Player 1: " + score1);
+        GUI.Label(new Rect(10, 20, 150, 30), "Player 2: " + score2);
+    }
+}
